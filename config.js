cfb49ed15221c7b27aafde2dd2d0722157b3af0b
d27t8v99 <dao.tvan99@gmail.com>
2024-05-12 23:43:44 +0700

    Add files via upload
    card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Linh';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100029163785876';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Trần Khánh Linh';
+const contentCard = 'Chúc em sinh nhật vui vẻ. Mong mọi điều tốt đẹp sẽ luôn đến với em';
+
+
+const giftImage = 'tkl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
